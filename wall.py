0b3ba871dc6adc5ba975e73aab28d8abdd94bd39
Shubham <132815243+versus2004@users.noreply.github.com>
2023-08-28 14:04:23 +0530

    Add files via upload

---
+++ wall.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import subprocess
+import os
+import random
+
+def set_gnome_wallpaper(file_path):
+    command = [
+        "gsettings",
+        "set",
+        "org.gnome.desktop.background",
+        "picture-uri",
+        f"'file://{file_path}'"
+    ]
+    try:
+        subprocess.run(command, check=True)
+        print("Wallpaper changed with success.")
+    except subprocess.CalledProcessError:
+        print("An error occurred while setting a new wallpaper.")
+
+if __name__ == '__main__':
+    wallpaper_directory = "/home/versus/Downloads/Walls/new"
+
+    # Get a list of all files with jpg or jpeg extension
+    image_files = [f for f in os.listdir(wallpaper_directory) if f.lower().endswith((".jpg", ".jpeg"))]
+
+    if image_files:
+        # Select a random image file from the list
+        random_image = random.choice(image_files)
+        image_path = os.path.join(wallpaper_directory, random_image)
+
+        set_gnome_wallpaper(image_path)
+    else:
+        print("No image files found in the directory.")
